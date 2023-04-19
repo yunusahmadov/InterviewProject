a5b5235a0d50ed8dd1e7b69b6886b18ed3133ae6
Yunus <yunus.ahmedov.11@mail.ru>
2023-04-19 22:45:38 +0400

    combined json and translator

--- src/components/Pages/PageElems/PageMain.jsx
+++ src/components/Pages/PageElems/PageMain.jsx
@@ -3,13 +3,28 @@ import Accordion from 'react-bootstrap/Accordion';
 import Card from 'react-bootstrap/Card';
 import AccordionElement from './AccordionElement';
 import '../../../index.css';
+import i18next from 'i18next';
+import enTranslation from '../../../../public/locales/en.json';
+import ruTranslation from '../../../../public/locales/ru.json';
+import { htmlQuestions } from '../../../data/data';
 
 function PageMain({ test:{questions}} ) {
   console.log(questions.title);
+  i18next.init({
+    lng: 'ru', // выберите язык, который будет использоваться по умолчанию
+    resources: {
+      en: {
+        translation: enTranslation, // объект с переводами для английского языка
+      },
+      ru: {
+        translation: ruTranslation, // объект с переводами для русского языка
+      },
+    },
+  });
   return (
     <div className="w-full h-auto">
       <div className="main-page ">
-        {
+        {/* {
           questions.map((item,index)=>{
             return(
               <div key={index}>
@@ -27,7 +42,16 @@ function PageMain({ test:{questions}} ) {
               </div>
             )
           })
+        } */
+        htmlQuestions.questions.map((question) => (
+          <div key={question.title}>
+            <h3>{i18next.t(question.title)}</h3>
+            <p>{i18next.t(question.answer)}</p>
+          </div>
+        ))
         }
+
+        
       </div>
     </div>
   );
