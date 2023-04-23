023697abd20b49dee5abe16afe6030182f548176
Yunus <yunus.ahmedov.11@mail.ru>
2023-04-23 12:54:47 +0400

    added the ability to write a description to the answers with images,and add more than 1 img

--- src/data/data.js
+++ src/data/data.js
@@ -1,74 +1,86 @@
-import htmlLogo from '../assets/htmllogo.png'
-import csslogo from '../assets/csslogo.png'
-import jslogo from '../assets/jslogo.png'
-import code1 from '../assets/code1.png'
+import htmlLogo from "../assets/htmllogo.png";
+import csslogo from "../assets/csslogo.png";
+import jslogo from "../assets/jslogo.png";
+import code1 from "../assets/code1.png";
+import jsq2 from "../assets/jsq2.png";
+import jsq3 from "../assets/jsq3.png";
+import jsq3var2 from "../assets/jsq3var2.png";
 
 
-const languages={
-    items:[
-        {
-            title:'HTML',
-            color: "from-orange-400 to-orange-600",
-            btnColor:"bg-orange-600",
-            btnShadow:"shadow-md shadow-orange-800",
-            shadow: "shadow-lg shadow-orange-600",
-            logo:htmlLogo,
-            link:'htmlpage'
-        },
-        {
-            title:'Css',
-            color: "from-sky-600 to-sky-500",
-            btnColor:"bg-sky-600",
-            btnShadow:"shadow-md shadow-sky-800",
-            shadow: "shadow-lg shadow-sky-500",
-            logo:csslogo,
-            link:'csspage'
-        },
-        {
-            title:'JavaScript',
-            color: "from-amber-300 to-amber-400 ",
-            btnColor:"bg-amber-400",
-            btnShadow:"shadow-md shadow-amber-600",
-            shadow: "shadow-lg shadow-yellow-500",
-            logo:jslogo,
-            link:'jspage'
-        },
-    ]
-}
 
-const htmlQuestions={
-   questions:[
+
+const languages = {
+  items: [
     {
-        title:'quest1',
-        answer:'answer1'
+      title: "HTML",
+      color: "from-orange-400 to-orange-600",
+      btnColor: "bg-orange-600",
+      btnShadow: "shadow-md shadow-orange-800",
+      shadow: "shadow-lg shadow-orange-600",
+      logo: htmlLogo,
+      link: "htmlpage",
     },
     {
-        title:'quest2',
-        answer:'answer2'
+      title: "Css",
+      color: "from-sky-600 to-sky-500",
+      btnColor: "bg-sky-600",
+      btnShadow: "shadow-md shadow-sky-800",
+      shadow: "shadow-lg shadow-sky-500",
+      logo: csslogo,
+      link: "csspage",
     },
+    {
+      title: "JavaScript",
+      color: "from-amber-300 to-amber-400 ",
+      btnColor: "bg-amber-400",
+      btnShadow: "shadow-md shadow-amber-600",
+      shadow: "shadow-lg shadow-yellow-500",
+      logo: jslogo,
+      link: "jspage",
+    },
+  ],
+};
 
-   ]
-}
-const cssQuestions={
-    questions:[
-     {
-         title:'cssq1',
-         answer:'cssq1answer',
-         img:code1
-     },
+const htmlQuestions = {
+  questions: [
+    {
+      title: "quest1",
+      answer: "answer1",
+    },
+    {
+      title: "quest2",
+      answer: "answer2",
+    },
+  ],
+};
+const cssQuestions = {
+  questions: [
+    {
+      title: "cssq1",
+      answer: "cssq1answer",
+      img: code1,
+    },
+  ],
+};
 
-    ]
- }
+const JsQuestions = {
+  questions: [
+    {
+      title: "jsq1",
+      answer: "jsq1answer",
+    },
+    {
+      title: "jsq2",
+      img: jsq2,
+    },
+    {
+        title: "jsq3",
+        img: jsq3,
+        description1:"jsq3desc",
+        description2:"jsq3desc2",
+        img2: jsq3var2,
+      },
+  ],
+};
 
- const JsQuestions={
-    questions:[
-     {
-         title:'jsq1',
-         answer:'jsq1answer',
-     },
-
-    ]
- }
-
-
-export {languages,htmlQuestions,cssQuestions,JsQuestions};
+export { languages, htmlQuestions, cssQuestions, JsQuestions };
